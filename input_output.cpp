4a6bc61fe80d4fc4d1cbce0cccf95873ee6884f7
sa9586997-dev <sa9586997@gmail.com>
2026-08-10 16:52:22 +0530

    Add input/output basics

---
+++ input_output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int age;
+    float hieght;
+    char grade;
+    string name;
+
+    cout <<"enter your age,hieght,grade,name:";
+    cin >> age >> hieght >> grade >> name;
+    
+    cout << "your age:" << age <<"\n";
+    cout << "your hieght:" << hieght <<"\n";
+    cout << "your grade :" <<grade<<"\n";
+    cout << "your name:;" << name <<"\n";
+ 
+    return 0;
+}
