1661cf716feb1dfda6758ad5ec67bace45bee086
sa9586997-dev <sa9586997@gmail.com>
2026-08-13 14:54:40 +0530

    Add whileloops

---
+++ whileloops.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void printNumbers () {
+    int count = 0;
+
+    // while loops  run while condition is true 
+    while (count  < 5){
+        cout << count << endl;
+        count ++;
+    }
+}
+int main (){
+    printNumbers ();
+     
+    return 0;
+}
